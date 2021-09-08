4870686b15578241d3351a1114924d13fe6770ea
aerdnas <71413484+aerdnas@users.noreply.github.com>
2021-09-09 01:17:27 +0200

    Created from v1.0 - Added more lights games

--- lightscontroller.ino
+++ lightscontroller.ino
@@ -1,4 +1,3 @@
-
 #include <RBDdimmer.h>
 
 #define USE_SERIAL  Serial
@@ -10,45 +9,148 @@ dimmerLamp dimmer4(11);
 dimmerLamp dimmer5(12);
 dimmerLamp dimmer6(13);
 
+unsigned short power = 100;  // from 0% to 100%
+unsigned long delayx = 125;
+unsigned long delayy = 100;
+unsigned long onesec = 1000;
+
 void setup() {
 
-  dimmer1.begin(NORMAL_MODE, ON); dimmer1.setPower(50);
-  dimmer2.begin(NORMAL_MODE, ON); dimmer2.setPower(50);
-  dimmer3.begin(NORMAL_MODE, ON); dimmer3.setPower(50);
-  dimmer4.begin(NORMAL_MODE, ON); dimmer4.setPower(50);
-  dimmer5.begin(NORMAL_MODE, ON); dimmer5.setPower(50);
-  dimmer6.begin(NORMAL_MODE, ON); dimmer6.setPower(50);
+  dimmer1.begin(NORMAL_MODE, ON); dimmer1.setPower(power);  //g
+  dimmer2.begin(NORMAL_MODE, ON); dimmer2.setPower(power);  //y
+  dimmer3.begin(NORMAL_MODE, ON); dimmer3.setPower(power);  //b
+  dimmer4.begin(NORMAL_MODE, ON); dimmer4.setPower(power);  //g
+  dimmer5.begin(NORMAL_MODE, ON); dimmer5.setPower(power);  //y
+  dimmer6.begin(NORMAL_MODE, ON); dimmer6.setPower(power);  //b
     
   Serial.begin(9600);
   Serial.println("end_setup");
-  
+
+  dimmer1.setState(ON); 
+  delay(onesec);
+  dimmer2.setState(ON);
+  delay(onesec);
+  dimmer3.setState(ON);
+  delay(onesec);
+  dimmer4.setState(ON);
+  delay(onesec);
+  dimmer5.setState(ON);
+  delay(onesec);
+  dimmer6.setState(ON);
+  delay(onesec);
+  delay(onesec);
+  dimmer1.setState(OFF); 
+  dimmer2.setState(OFF);
+  dimmer3.setState(OFF);
+  dimmer4.setState(OFF);
+  dimmer5.setState(OFF);
+  dimmer6.setState(OFF);
 }
 
 void loop() {
 
-  dimmer1.setState(ON); 
-  delay(100);
-  dimmer2.setState(ON);
-  delay(100);
-  dimmer3.setState(ON);
-  delay(100);
-  dimmer4.setState(ON);
-  delay(100);
-  dimmer5.setState(ON);
-  delay(100);
-  dimmer6.setState(ON);
-  delay(100);
-  dimmer1.setState(OFF); 
-  delay(100);
-  dimmer2.setState(OFF);
-  delay(100);
-  dimmer3.setState(OFF);
-  delay(100);
-  dimmer4.setState(OFF);
-  delay(100);
-  dimmer5.setState(OFF);
-  delay(100);
-  dimmer6.setState(OFF);
-  delay(100);
+  for(int i = 0; i < .10; i++){
+    dimmer1.setState(ON); 
+    delay(delayy);
+    dimmer2.setState(ON);
+    delay(delayy);
+    dimmer3.setState(ON);
+    delay(delayy);
+    dimmer4.setState(ON);
+    delay(delayy);
+    dimmer5.setState(ON);
+    delay(delayy);
+    dimmer6.setState(ON);
+    delay(delayy);
+    dimmer1.setState(OFF); 
+    delay(delayy);
+    dimmer2.setState(OFF);
+    delay(delayy);
+    dimmer3.setState(OFF);
+    delay(delayy);
+    dimmer4.setState(OFF);
+    delay(delayy);
+    dimmer5.setState(OFF);
+    delay(delayy);
+    dimmer6.setState(OFF);
+    delay(delayy);
+  }
+  
+  for(int i = 0; i < .10; i++){
+    dimmer3.setState(ON);
+    dimmer6.setState(ON);
+    delay(delayy);
+    dimmer1.setState(OFF); 
+    dimmer4.setState(OFF);
+    delay(delayy); 
+    dimmer2.setState(ON);
+    dimmer5.setState(ON);
+    delay(delayy); 
+    dimmer3.setState(OFF);
+    dimmer6.setState(OFF);
+    delay(delayy);
+    dimmer1.setState(ON); 
+    dimmer4.setState(ON);
+    delay(delayy);
+    dimmer2.setState(OFF);
+    dimmer5.setState(OFF);
+    delay(delayy);
+  }
+
+  for(int i = 0; i < .10; i++){
+    dimmer1.setState(ON);
+    dimmer6.setState(OFF);
+    delay(delayy);
+    dimmer2.setState(ON);
+    dimmer1.setState(OFF);
+    delay(delayy);
+    dimmer3.setState(ON);
+    dimmer2.setState(OFF);
+    delay(delayy);
+    dimmer4.setState(ON);
+    dimmer3.setState(OFF);
+    delay(delayy); 
+    dimmer5.setState(ON);
+    dimmer4.setState(OFF);
+    delay(delayy);
+    dimmer6.setState(ON);
+    dimmer5.setState(OFF); 
+    delay(delayy);
+  }
+
+  for(int i = 0; i < .20; i++){
+    dimmer1.setState(ON);
+    dimmer2.setState(ON);
+    dimmer3.setState(ON);
+    dimmer4.setState(OFF);
+    dimmer5.setState(OFF);
+    dimmer6.setState(OFF);
+    delay(delayx);
+    dimmer4.setState(ON);
+    dimmer5.setState(ON);
+    dimmer6.setState(ON);
+    dimmer1.setState(OFF);
+    dimmer2.setState(OFF);
+    dimmer3.setState(OFF);
+    delay(delayx);
+  }
+  
+  for(int i = 0; i < .20; i++){
+    dimmer1.setState(ON);
+    dimmer6.setState(ON);
+    dimmer3.setState(OFF); 
+    dimmer4.setState(OFF);
+    delay(delayx); 
+    dimmer2.setState(ON);
+    dimmer5.setState(ON);
+    dimmer1.setState(OFF);
+    dimmer6.setState(OFF);
+    delay(delayx);
+    dimmer3.setState(ON); 
+    dimmer4.setState(ON);
+    dimmer2.setState(OFF);
+    dimmer5.setState(OFF);
+    delay(delayx);
+  }
 
 }
